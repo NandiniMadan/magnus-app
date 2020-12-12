e882dfc7e23e499ca1a1509862fef2bf3ffc85aa
NandiniMadan <nandunandana999@gmail.com>
2020-12-12 22:10:03 +0530

    Intial Commit

---
+++ backend/express-server/routes.js
@@ -0,0 +1,8 @@
+'use strict'
+module.exports = function (app, opts) {
+  // Setup routes, middleware, and handlers
+  app.get('/', (req, res) => {
+    res.locals.name = 'express-server'
+    res.render('index')
+  })
+}
