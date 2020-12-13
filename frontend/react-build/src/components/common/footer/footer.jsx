e012d2f2c3f36a9482433e5ebfcdc30bd94925ad
NandiniMadan <nandunandana999@gmail.com>
2020-12-13 12:13:17 +0530

    Added sample Header and Footer in React Package

--- frontend/react-build/src/components/common/footer/footer.jsx
+++ frontend/react-build/src/components/common/footer/footer.jsx
@@ -4,7 +4,7 @@ export default class footer extends Component {
     render() {
         return (
             <div>
-                
+                <h1>I am Footer</h1>
             </div>
         )
     }
